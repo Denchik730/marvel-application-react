76934dd33d0088f0466d03b76a608c7960744c24
Denis Gabbasov <gabbas.denn@mail.ru>
2023-02-10 18:03:35 +0300

    feat:реализовать обращение сервера за случайным персонажем и трансформировать приходяшие данные в удобный формат

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,9 @@
 
 
 class MarvelService {
+  _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+  _apiKey = 'apikey=8eba837de29e2ca3122d673d4dd7e5a2';
+
   getResource = async (url) => {
     let res = await fetch(url);
 
@@ -11,8 +14,26 @@ class MarvelService {
     return await res.json();
   }
 
-  getAllCharacters = () => {
-    return this.getResource('https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=900&apikey=8eba837de29e2ca3122d673d4dd7e5a2')
+  getAllCharacters = async () => {
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=900&${this._apiKey}`);
+    console.log(res)
+
+    return res.data.results.map(this._transformCharacter);
+  }
+
+  getCharacter = async (id) => {
+    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    return this._transformCharacter(res.data.results[0]);
+  }
+
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.url,
+      wiki: char.url
+    }
   }
 }
 
